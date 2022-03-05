2fdf2684f717a6adbb893c2321e6079a333bfaa9
Sarah Nosal <nosalsm@gmail.com>
2022-03-04 18:23:04 -0600

    made it and ate it pages mobile friendly

--- client/src/MadeIt.js
+++ client/src/MadeIt.js
@@ -17,7 +17,7 @@ function MadeIt({bakedGoods}){
                             <span className='foodName'>{bakedGood.name}</span><hr id='hr'/>
                             <span className='foodPrice'>It's yours for: ${bakedGood.price}</span><br /><br />
                             <span className='foodDescription'>Description: {bakedGood.description}</span><br /><br /><br /><br />
-                            <Link className='review-link' to={`/made-it/${bakedGood.id}`} >See what people are saying!</Link>
+                            <Link className='review-link' to={`/made-it/${bakedGood.id}`} >See the reviews!</Link>
                         </Box>
                    </Box> 
                 ))}
